ab4686c9732fbfd670f80694d5042f8dc5732522
Anthony Afetsrom <sirantho20@gmail.com>
2015-02-12 14:03:25 +0000

    delete

---
+++ delete.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tony
+ * Date: 2/11/15
+ * Time: 4:38 PM
+ */
+require_once 'Getvcard.php';
+if(!$file = @file('vcards.txt', FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES))
+{
+    echo 'You must first execute the file getpath.sh'.PHP_EOL;
+}
+else
+{
+
+
+foreach($file as $path)
+{
+    $cur = substr($path,0,-12);
+    if(is_file(trim($cur)))
+    {
+        //$obj = (new Getvcard($cur))->insertContact();
+        unlink($cur);
+    }
+    else
+    {
+        echo 'file not found'.PHP_EOL;
+    }
+}
+
+}
